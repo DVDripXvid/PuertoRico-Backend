2680bfd8f10e06434cd1f406e64a7557514324a9
Rottenhoffer Oliver <dvdrip.xvid.avi@gmail.com>
2021-01-16 23:52:40 +0100

    Redis migration (#1)
    * replace cosmosdb with redis
    
    * parse redis url
    
    Co-authored-by: Olivér Rottenhoffer <orottenhoffer@geomant.com>

--- PuertoRico.Engine/Services/ReplayableGameService.cs
+++ PuertoRico.Engine/Services/ReplayableGameService.cs
@@ -12,39 +12,47 @@ namespace PuertoRico.Engine.Services
     {
         private readonly IGameService _gameService;
         private readonly IGameRepository _repository;
-        
-        public ReplayableGameService(IGameRepository repository, IGameService gameService) {
+
+        public ReplayableGameService(IGameRepository repository, IGameService gameService)
+        {
             _repository = repository;
             _gameService = gameService;
         }
 
-        public async Task ExecuteRoleAction(Game game, string userId, IAction action) {
+        public async Task ExecuteRoleAction(Game game, string userId, IAction action)
+        {
             await _gameService.ExecuteRoleAction(game, userId, action);
             await _repository.AddAction(game.Id, userId, action);
         }
 
-        public async Task ExecuteSelectRole(Game game, string userId, SelectRole selectRole) {
+        public async Task ExecuteSelectRole(Game game, string userId, SelectRole selectRole)
+        {
             await _gameService.ExecuteSelectRole(game, userId, selectRole);
             await _repository.AddAction(game.Id, userId, selectRole);
         }
 
-        public Task<IEnumerable<ActionType>> GetAvailableActionTypeForUser(Game game, string userId) {
+        public Task<IEnumerable<ActionType>> GetAvailableActionTypeForUser(Game game, string userId)
+        {
             return _gameService.GetAvailableActionTypeForUser(game, userId);
         }
 
-        public async Task<IEnumerable<PlayerResultDto>> HandleFinishedGame(Game game) {
-            var results = game.Players.Select(p => new PlayerResultDto {
+        public async Task<IEnumerable<PlayerResultDto>> HandleFinishedGame(Game game)
+        {
+            var results = game.Players.Select(p => new PlayerResultDto
+            {
                 Player = PlayerDto.Create(p),
                 Result = p.CalculateVictoryPoints()
             }).ToList();
 
             var entity = GameEntity.Create(game);
-            foreach (var player in entity.Players) {
+            foreach (var player in entity.Players)
+            {
                 player.Result = results.First(r => r.Player.UserId == player.UserId).Result;
             }
-            entity.ttl = 60 * 60 * 24 * 7;  // Delete finished in a week
+            // TODO: use redis ttl
+            // entity.ttl = 60 * 60 * 24 * 7;  // Delete finished in a week
             await _repository.ReplaceGame(entity);
-            
+
             return results;
         }
     }
--- PuertoRico.Engine/Startup.cs
+++ PuertoRico.Engine/Startup.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.Azure.Cosmos.Fluent;
-using Microsoft.Azure.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,24 +11,33 @@ using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using PuertoRico.Engine.DAL;
+using PuertoRico.Engine.DAL.Redis;
+using PuertoRico.Engine.Extensions;
 using PuertoRico.Engine.Services;
-using PuertoRico.Engine.SignalR;
 using PuertoRico.Engine.SignalR.Hubs;
 using PuertoRico.Engine.Stores;
 using PuertoRico.Engine.Stores.InMemory;
+using StackExchange.Redis;
 
 namespace PuertoRico.Engine
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration) {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Objects
+            };
+
             services.AddMvc();
 
             services.AddApplicationInsightsTelemetry();
@@ -41,36 +48,39 @@ namespace PuertoRico.Engine
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .WithOrigins(
-                        "http://localhost:3000",
-                        "https://storagepuertodev.z16.web.core.windows.net",
-                        "https://storagepuertodevstage.z16.web.core.windows.net"
+                        "http://localhost:3000"
                     )
                 ));
 
             AddSignalR(services)
                 .AddJsonProtocol(c => c.PayloadSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
-            AddCosmos(services);
+            var redisConfig = new ConfigurationOptions().ParseUrl(Configuration["REDIS_URL"]);
+            services.AddSingleton(ConnectionMultiplexer.Connect(redisConfig));
 
             services.AddTransient<IGameService, GameService>();
             services.AddTransient<IReplayableGameService, ReplayableGameService>();
             services.AddSingleton<IInProgressGameStore, InMemoryInProgressGameStore>();
             services.AddSingleton<IUserService, AuthenticatedUserService>();
 
-            services.AddSingleton<IGameRepository, CosmosGameRepository>();
+            services.AddSingleton<IGameRepository, RedisGameRepository>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(jwt => {
+                .AddJwtBearer(jwt =>
+                {
                     jwt.Audience = "178792157062-b5jtd265enrjn20s04hqtfntm9esshrf.apps.googleusercontent.com";
                     jwt.Authority = "https://accounts.google.com";
-                    jwt.TokenValidationParameters = new TokenValidationParameters {
+                    jwt.TokenValidationParameters = new TokenValidationParameters
+                    {
                         ValidateIssuer = true,
-                        ValidIssuers = new[] {"https://accounts.google.com", "accounts.google.com"},
+                        ValidIssuers = new[] { "https://accounts.google.com", "accounts.google.com" },
                         ValidateAudience = true,
                         ValidAudience = "178792157062-b5jtd265enrjn20s04hqtfntm9esshrf.apps.googleusercontent.com",
                     };
-                    jwt.Events = new JwtBearerEvents {
-                        OnMessageReceived = ctx => {
+                    jwt.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = ctx =>
+                        {
                             var accessToken = ctx.Request.Query["access_token"];
                             ctx.Token = accessToken;
                             return Task.CompletedTask;
@@ -80,8 +90,10 @@ namespace PuertoRico.Engine
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public virtual void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory) {
-            if (env.IsDevelopment()) {
+        public virtual void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
             }
 
@@ -90,7 +102,8 @@ namespace PuertoRico.Engine
 
             app.UseRouting();
             app.UseAuthorization();
-            app.UseEndpoints(endpoints => {
+            app.UseEndpoints(endpoints =>
+            {
                 endpoints.MapHub<LobbyHub>("lobby");
                 endpoints.MapHub<GameHub>("game");
             });
@@ -100,21 +113,8 @@ namespace PuertoRico.Engine
             scope.ServiceProvider.GetService<IInProgressGameStore>();
         }
 
-        protected virtual void AddCosmos(IServiceCollection services) {
-            var jsonSettings = new JsonSerializerSettings {
-                TypeNameHandling = TypeNameHandling.Objects
-            };
-            var client = new CosmosClientBuilder(
-                    Configuration["Azure:Cosmos:Endpoint"],
-                    Configuration["Azure:Cosmos:Key"])
-                .WithConnectionModeDirect()
-                .WithCustomSerializer(new NewtonsoftJsonCosmosSerializer(jsonSettings))
-                .Build();
-
-            services.AddSingleton(client);
-        }
-
-        protected virtual ISignalRServerBuilder AddSignalR(IServiceCollection services) {
+        protected virtual ISignalRServerBuilder AddSignalR(IServiceCollection services)
+        {
             return services.AddSignalR()
                 /*.AddAzureSignalR(opts => {
                     opts.ServerStickyMode = ServerStickyMode.Required;
